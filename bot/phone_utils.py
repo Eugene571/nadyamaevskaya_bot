bce1ccbaa6af831a16a98452eda93e11d1f69c9d
Eugene Pisarev <zm1s3ry@gmail.com>
2025-04-19 13:10:43 +0300

    accepting all phonenums

--- bot/phone_utils.py
+++ bot/phone_utils.py
@@ -3,8 +3,8 @@ import phonenumbers
 
 def normalize_phone_number(raw_phone: str) -> str | None:
     try:
-        phone = phonenumbers.parse(raw_phone, "RU")
+        phone = phonenumbers.parse(raw_phone)  # Библиотека сама определит страну
         if phonenumbers.is_valid_number(phone):
-            return phonenumbers.format_number(phone, phonenumbers.PhoneNumberFormat.E164)  # +7XXXXXXXXXX
+            return phonenumbers.format_number(phone, phonenumbers.PhoneNumberFormat.E164)
     except phonenumbers.NumberParseException:
         return None
