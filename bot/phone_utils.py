1c61692cdac79a7ba13140f2db11befda47cbe54
Eugene Pisarev <zm1s3ry@gmail.com>
2025-04-19 20:49:51 +0300

    processing RU and other countries phones

--- bot/phone_utils.py
+++ bot/phone_utils.py
@@ -1,10 +1,14 @@
 import phonenumbers
 
-
 def normalize_phone_number(raw_phone: str) -> str | None:
     try:
-        phone = phonenumbers.parse(raw_phone)  # Библиотека сама определит страну
+        if raw_phone.startswith('+'):
+            phone = phonenumbers.parse(raw_phone, None)  # None = автоопределение по коду страны
+        else:
+            phone = phonenumbers.parse(raw_phone, "RU")  # Локальные без + считаем как RU
+
         if phonenumbers.is_valid_number(phone):
             return phonenumbers.format_number(phone, phonenumbers.PhoneNumberFormat.E164)
+
     except phonenumbers.NumberParseException:
         return None
